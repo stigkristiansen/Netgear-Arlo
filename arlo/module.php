c531c4de2ebe40d4859d8fc8be67873aab077248
stigkristiansen <stigkristiansen@outlook.com>
2018-02-20 20:00:37 +0100

    .

--- arlo/module.php
+++ arlo/module.php
@@ -22,17 +22,36 @@ class ArloModule extends IPSModule {
 		
 		if(strlen($password)>0 && strlen($email)>0) {
 			$arlo = new Arlo();
-			if (!$arlo->Init($email,$password))
+			if ($arlo->Init($email,$password)===false)
 				return false;
-			if(!$arlo->StartStream($CameraName))
+			if($arlo->StartStream($CameraName)===false)
 				return false;;
-			if(!$arlo->TakeSnapshot($CameraName))
-				return false;;
+			if($arlo->TakeSnapshot($CameraName)===false)
+				return false;
 			
 			$arlo->StopStream($CameraName);
 			$arlo->Logout();
 			
 			return true;
+		}	
+	}
+	
+	public function GetLibrary ($FromYYYYMMDD, $ToYYYYMMDD) {
+		$email = $this->ReadPropertyString("email");
+		$password = $this->ReadPropertyString("password");
+		
+		if(strlen($password)>0 && strlen($email)>0) {
+			$arlo = new Arlo();
+			if (!$arlo->Init($email,$password))
+				return false;
+			
+			$library = $arlo->GetLibrary($FromYYYYMMDD, $ToYYYYMMDD);
+			$arlo->Logout();
+			
+			if($library === false)
+				return false;
+					
+			return $library;
 		}	
 	}
 	
