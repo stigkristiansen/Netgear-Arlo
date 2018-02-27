1564d4f50234391b48c64110f8942967e7c07b13
stigkristiansen <stigkristiansen@outlook.com>
2018-02-27 19:18:41 +0100

    .

--- camera/module.php
+++ camera/module.php
@@ -7,23 +7,26 @@ class ArloCameraModule extends IPSModule {
     public function Create(){
         parent::Create();
         
-        $this->RegisterPropertyBoolean ("Log", true);
-		$this->RegisterPropertyBoolean ("DeleteImage", true);
-		$this->RegisterPropertyInteger ("ArloModuleInstanceId", 0);
-		$this->RegisterPropertyString ("ArloCameraName", "");
-		$this->RegisterPropertyString ("ArloCameraDeviceId", "");
+        $this->RegisterPropertyBoolean("Log", true);
+		$this->RegisterPropertyBoolean("DeleteImage", true);
+		$this->RegisterPropertyInteger("ArloModuleInstanceId", 0);
+		$this->RegisterPropertyString("ArloCameraName", "");
+		$this->RegisterPropertyString("ArloCameraDeviceId", "");
+		$this->RegisterPropertyBoolean("ScheduleSnapshot", false);
 	}
    
     public function ApplyChanges(){
         parent::ApplyChanges();
 		
 		$script = "<? NAC_TakeSnapshot(".$this->InstanceID."); ?>";
-		//file_get_contents(__DIR__ . "/../libs/_Snapshot.php");
 		$scriptId = $this->RegisterScript("scriptsnapshot", "_Snapshot", $script, 0);
 		
 		$eventId = IPS_CreateEvent(1);
 		IPS_SetParent($eventId, $scriptId);
-		IPS_SetEventCyclic($eventId, 2 /* Täglich */, 1 /* Jeden Tag */, 0, 0, 3 /* Stündlich */, 12 /* Alle 12 Stunden */);
+		IPS_SetEventCyclicTimeFrom($eventId , 15 , 0, 0) ;
+		
+		IPS_SetEventActive($eventId,$this->ReadPropertyBoolean("ScheduleSnapshot")); 
+		
     }
 	
 	public function TakeSnapshot() {
