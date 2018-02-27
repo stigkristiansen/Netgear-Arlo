ad50f6b9e12205c6c84f0a381d9bb9f60adf46b3
stigkristiansen <stigkristiansen@outlook.com>
2018-02-27 20:20:18 +0100

    .

--- camera/module.php
+++ camera/module.php
@@ -49,8 +49,7 @@ class ArloCameraModule extends IPSModule {
 		$url = "";
 		for($x=0;$x<Count($library);$x++) {
 			$lastModified = $library[$x]->lastModified;
-			if($library[$x]->deviceId==$cameraDeviceId && $lastModified > $now && $lastModified < $now+10000) {
-				//$url = $library[$x]->presignedContentUrl; 
+			if($library[$x]->deviceId==$cameraDeviceId && $lastModified > $now) {
 				$item = $library[$x];
 				break;
 			}
@@ -58,6 +57,7 @@ class ArloCameraModule extends IPSModule {
 		
 		if(isset($item)) {
 			$filename = __DIR__ . "/../../../media/".$cameraName.".jpg";
+			
 			if(NA_DownloadURL($InstanceId, $item->presignedContentUrl, $filename)) {
 				$imgId = IPS_GetObjectIDByIdent($cameraName."Snapshot", $this->InstanceID);
 				if($imgId!==false)
