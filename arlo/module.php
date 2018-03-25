e913a76d928e1d3ac1875e6405ed694f0096578a
stigkristiansen <stigkristiansen@outlook.com>
2018-03-25 17:04:07 +0200

    .

--- arlo/module.php
+++ arlo/module.php
@@ -128,9 +128,8 @@ class ArloModule extends IPSModule {
 							IPS_ApplyChanges($cameraInsId);
 							
 							$data = array("Command"=>"UpdateScheduledTime", "Offset"=>$y); 
-							SendToChildren($cameras[$y]->deviceId, $data);
-							// Test
-							
+							$this->SendToChildren($cameras[$y]->deviceId, $data);
+														
 							$log->LogMessage("Creating image for camera ".$cameras[$y]->deviceName);
 							$imgId = $this->CreateMediaByName($cameraInsId, "Snapshot", 1, $cameras[$y]->deviceId);
 							$filename = __DIR__ . "/../../../media/".$cameras[$y]->deviceName.".jpg";
